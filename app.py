4fc6a537e87e030ccef7a4e8b322363c93497623
StarFox144 <r.babenko@istu.edu.ua>
2024-10-17 13:44:50 +0300

    code

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
+app=Flask(__name__)
+api=Api(app)
 
+items=[]
+
+class item(Resource):
+    def get(self,name):
+        for items in items:
+            if team['name']==name:
+                return jsonify(item)
+            return {'message':'item not found'},404
+    
+    def post(self):
+        data = request.get_json()
+        new_item={
+            'name':data['name'],
+            'price':data['price']
+        }
+        items.append(new_item)
+        return jsonity(new_item)
+
+    def delete(self,name):
+        global items
+        items=[items for item in item if item['name']!=name]
+        return{'message':'Item delted'}    
+
+api.add_resource(Item, '/item/<string:name>','/item')
+
+if __name__=='__main__':
+    app.run(debug=True)
